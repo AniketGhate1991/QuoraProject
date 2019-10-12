42e12878f489b0f1a4bd7ba4ec654bfdf1fd8873
AniketGhate1991 <aniket@zsoftware.co.in>
2019-10-12 19:59:59 +0530

    Intial Commit: UserEntityClass And UserDao Added

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,39 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity createUser(UserEntity userEntity){
+        entityManager.persist(userEntity);
+        return userEntity;
+    }
+
+    public UserEntity getUserName(final String Username){
+        try
+        {
+           return entityManager.createNamedQuery("userByUserName",UserEntity.class).setParameter("username",Username)
+                   .getSingleResult();
+        }
+        catch (NoResultException ex){
+            return null;
+        }
+    }
+
+    public UserEntity getUserByEmail(final String email) {
+        try {
+            return entityManager.createNamedQuery("userByEmail", UserEntity.class).setParameter("email", email).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+}
