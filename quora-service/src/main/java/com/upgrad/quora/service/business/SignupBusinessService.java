e8154f36b67af19e775bb54735c36629a7010867
AniketGhate1991 <aniket@zsoftware.co.in>
2019-10-19 19:11:18 +0530

    UserBusinessService Added

--- quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
@@ -23,6 +23,8 @@ public class SignupBusinessService {
 
         if (userEntity != null) {
             throw new SignUpRestrictedException("SGR-001", "Try any other Username, this Username has already been taken");
+
+
         }
 
         UserEntity userEntitys =  userDao.getUserByEmail(userentity.getEmail());
